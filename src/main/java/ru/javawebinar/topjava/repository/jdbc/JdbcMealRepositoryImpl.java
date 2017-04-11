0ea21382ea7c28a800654a036dc8c3bb2aba6e3b
vitchurb <vitchurb@gmail.com>
2017-04-11 23:51:22 +0400

    HW3

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
@@ -1,37 +1,83 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.sql.DataSource;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 public class JdbcMealRepositoryImpl implements MealRepository {
 
+    private static final BeanPropertyRowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
+
+    private final JdbcTemplate jdbcTemplate;
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private final SimpleJdbcInsert insertMeal;
+
+    @Autowired
+    public JdbcMealRepositoryImpl(DataSource dataSource, JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.insertMeal = new SimpleJdbcInsert(dataSource)
+                .withTableName("meals")
+                .usingGeneratedKeyColumns("id");
+        this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        MapSqlParameterSource map = new MapSqlParameterSource()
+                .addValue("id", meal.getId())
+                .addValue("datetime", meal.getDateTime())
+                .addValue("description", meal.getDescription())
+                .addValue("calories", meal.getCalories())
+                .addValue("userId", userId);
+
+        if (meal.isNew()) {
+            Number newKey = insertMeal.executeAndReturnKey(map);
+            meal.setId(newKey.intValue());
+            return meal;
+        } else {
+            return namedParameterJdbcTemplate.update(
+                    "UPDATE meals SET datetime=:datetime, description=:description, calories=:calories " +
+                            " WHERE id=:id AND user_id=:userId", map) > 0 ? meal : null;
+        }
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return jdbcTemplate.update("DELETE FROM meals WHERE id=? AND user_id=?", id, userId) != 0;
+
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        List<Meal> meals = jdbcTemplate.query("SELECT * FROM meals WHERE id=? AND user_id=?", ROW_MAPPER, id, userId);
+        return DataAccessUtils.singleResult(meals);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return jdbcTemplate.query("SELECT * FROM meals WHERE user_id=? ORDER BY datetime DESC", ROW_MAPPER, userId);
     }
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        Objects.requireNonNull(startDate);
+        Objects.requireNonNull(endDate);
+        return jdbcTemplate.query("SELECT * FROM meals WHERE user_id=? " +
+                " AND datetime BETWEEN ? AND ? ORDER BY datetime DESC", ROW_MAPPER, userId, startDate, endDate);
     }
 }
--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -3,8 +3,29 @@ package ru.javawebinar.topjava;
 import ru.javawebinar.topjava.matcher.ModelMatcher;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+
+import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
+
 public class MealTestData {
+    public static final int USER_ID = START_SEQ;
+    public static final int ADMIN_ID = START_SEQ + 1;
+    public static final int MEAL_USER_ID_1 = START_SEQ + 2;
+    public static final int MEAL_USER_ID_2 = START_SEQ + 3;
+    public static final int MEAL_USER_ID_3 = START_SEQ + 4;
+    public static final int MEAL_ADMIN_ID_1 = START_SEQ + 5;
+    public static final int MEAL_ADMIN_ID_2 = START_SEQ + 6;
 
-    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>();
+    public static final Meal MEAL_USER_1 = new Meal(MEAL_USER_ID_1,
+            LocalDateTime.parse("2017-01-10T13:42:00"), "Завтрак", 515);
+    public static final Meal MEAL_USER_2 = new Meal(MEAL_USER_ID_2,
+            LocalDateTime.parse("2017-01-11T17:02:00"), "Обед", 1054);
+    public static final Meal MEAL_USER_3 = new Meal(MEAL_USER_ID_3,
+            LocalDateTime.parse("2017-01-11T21:58:00"), "Ужин", 101);
+    public static final Meal MEAL_ADMIN_1 = new Meal(MEAL_ADMIN_ID_1,
+            LocalDateTime.parse("2017-01-10T10:03:00"), "Завтрак", 500);
+    public static final Meal MEAL_ADMIN_2 = new Meal(MEAL_ADMIN_ID_2,
+            LocalDateTime.parse("2017-01-11T11:00:00"), "Завтрак", 700);
 
+    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher();
 }
