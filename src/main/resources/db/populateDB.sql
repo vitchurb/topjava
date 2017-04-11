fc8968b15a2819abd257aa752dea4b267269211f
vitchurb <vitchurb@gmail.com>
2017-04-12 00:50:02 +0400

    HW_3

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -11,3 +11,11 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (dateTime, description, calories, user_id) VALUES
+  ('20170110 13:42', 'Завтрак', 515, 100000),
+  ('20170111 17:02', 'Обед', 1054, 100000),
+  ('20170111 21:58', 'Ужин', 101, 100000),
+  ('20170110 10:03', 'Завтрак', 500, 100001),
+  ('20170111 11:00', 'Завтрак', 700, 100001)
+
