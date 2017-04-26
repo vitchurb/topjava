6d64790e17d997f7060bf0d5adc89c20c46ed1f0
vitchurb <vitchurb@gmail.com>
2017-04-26 09:52:34 +0400

    HW 05 - small changes

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -19,9 +19,6 @@ public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
     @Query("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
     int delete(@Param("id") int id, @Param("userId") int userId);
 
-    @Override
-    Meal findOne(Integer id);
-
     Meal findByUserIdAndId(int userId, int id);
 
     List<Meal> findByUserIdOrderByDateTimeDesc(int userId);
