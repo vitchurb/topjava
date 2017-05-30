88f3760ee9f91abb511fbbf4ecf52727f0c89f44
vitchurb <vitchurb@gmail.com>
2017-05-30 23:45:30 +0400

    HW9 - small changes

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -1,10 +1,7 @@
 package ru.javawebinar.topjava.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.javawebinar.topjava.service.MealService;
 
 @Controller
 public class RootController {
@@ -25,7 +22,7 @@ public class RootController {
     }
 
     @GetMapping("/meals")
-    public String meals(Model model) {
+    public String meals() {
         return "meals";
     }
 }
